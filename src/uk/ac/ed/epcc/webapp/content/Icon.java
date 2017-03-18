abe8782b677475e3051423eb4df2391739fafc71
Stephen Booth <s.booth@epcc.ed.ac.uk>
2017-03-18 21:37:54 +0000

    degrade in non servlet context

--- src/uk/ac/ed/epcc/webapp/content/Icon.java
+++ src/uk/ac/ed/epcc/webapp/content/Icon.java
@@ -2,9 +2,20 @@ package uk.ac.ed.epcc.webapp.content;
 
 import uk.ac.ed.epcc.webapp.AppContext;
 import uk.ac.ed.epcc.webapp.servlet.ServletService;
-
+/** A {@link UIGenerator} that adds an icon image.
+ * The image is specified as a url so the alt-text is used in a non-web context.
+ * 
+ * @author spb
+ *
+ */
 public class Icon implements UIGenerator {
 
+	/** 
+	 * 
+	 * @param conn {@link AppContext}
+	 * @param text String alt-text
+	 * @param image url of icon image
+	 */
 	public Icon(AppContext conn,String text, String image) {
 		super();
 		this.conn=conn;
@@ -22,14 +33,28 @@ public class Icon implements UIGenerator {
 	public ContentBuilder addContent(ContentBuilder builder) {
 		if( builder instanceof HtmlPrinter){
 			HtmlPrinter p = (HtmlPrinter) builder;
-			p.open("img");
-			p.attr("alt", text);
-			p.attr("src",conn.getService(ServletService.class).encodeURL(image));
-			p.close();
+			addImage(p);
 		}else{
-			builder.addText(text);
+			ExtendedXMLBuilder printer = builder.getSpan();
+			addImage(printer);
+			printer.appendParent();
 		}
 		return builder;
+	}
+	/**
+	 * @param p
+	 */
+	private void addImage(ExtendedXMLBuilder p) {
+		ServletService service = conn.getService(ServletService.class);
+		if( service != null){
+			p.open("img");
+			p.attr("class","icon");
+			p.attr("alt", text);
+			p.attr("src",service.encodeURL(image));
+			p.close();
+		}else{
+			p.clean(text);
+		}
 	}
 	public String toString(){
 		return text;
