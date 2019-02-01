faff8c2f22fb120b60808caaa1dc0e308ca830b1
Stephen Booth <s.booth@epcc.ed.ac.uk>
2019-02-01 16:38:24 +0000

    use null for anonymise

--- src/uk/ac/ed/epcc/webapp/session/WebNameFinder.java
+++ src/uk/ac/ed/epcc/webapp/session/WebNameFinder.java
@@ -41,7 +41,7 @@ public class WebNameFinder<AU extends AppUser> extends FieldNameFinder<AU,WebNam
 	 */
 	@Override
 	public void anonymise(AU target) {
-		getRecord(target).setProperty(WEB_NAME, "WebName"+target.getID());
+		getRecord(target).setProperty(WEB_NAME, null);
 	}
 
 	@Override
