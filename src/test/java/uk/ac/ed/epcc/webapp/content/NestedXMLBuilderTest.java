805d4b76aabe3a1daa9fa5d83bc2e0a0e63549f7
Stephen Booth <s.booth@epcc.ed.ac.uk>
2019-05-20 14:48:23 +0100

    fix test for well formed XML

--- src/test/java/uk/ac/ed/epcc/webapp/content/NestedXMLBuilderTest.java
+++ src/test/java/uk/ac/ed/epcc/webapp/content/NestedXMLBuilderTest.java
@@ -15,8 +15,7 @@ package uk.ac.ed.epcc.webapp.content;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -41,6 +40,7 @@ public class NestedXMLBuilderTest {
 			Assert.fail("Exception while creating NestedXMLBuilder.");
 		}
 		Assert.assertNotNull(domBuilder);
+		domBuilder.open("data");
 		// constructs DOM document
 		domBuilder.open("tag1", new String[][]{{"attr1_name", "attr1_value"}});
 		domBuilder.clean("test text");
@@ -53,12 +53,14 @@ public class NestedXMLBuilderTest {
 		nested.clean(123);
 		nested.close();
 		nested.appendParent();
+		domBuilder.close();
 		// converts DOM to SimpleXMLBuilder
 		XMLPrinter printer = new XMLPrinter();
 		domBuilder.addContent(printer);
-		Assert.assertEquals(
+		Assert.assertEquals("<data>"+
 				"<tag1 attr1_name='attr1_value'>test text</tag1>" +
-				"<tag2 attr2_name='attr2_value'>z123</tag2>",
+				"<tag2 attr2_name='attr2_value'>z123</tag2>" +
+				"</data>",
 				printer.toString());
 	}
 }
