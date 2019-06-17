564655d05b9c641580e60ea43d927185c6077b8e
Stephen Booth <s.booth@epcc.ed.ac.uk>
2019-06-17 16:11:19 +0100

    additional Transform MillisToHourTransform

--- src/main/java/uk/ac/ed/epcc/webapp/content/MillisToHourTransform.java
+++ src/main/java/uk/ac/ed/epcc/webapp/content/MillisToHourTransform.java
@@ -16,25 +16,19 @@
  *******************************************************************************/
 package uk.ac.ed.epcc.webapp.content;
 
-import uk.ac.ed.epcc.webapp.model.data.Duration;
-/** A {@link NumberTransform} that format a second-count into a
+/** A {@link NumberTransform} that format a millisecond-count into a
  * H:MM:SS format.
  * 
- * If the input is a {@link Duration} then the second value will be taken
- * (even though the default numerical value of a {@link Duration} is millisecond. However as it is easy to
- * lose the duration nature of a value (for example by taking averages) it is better to use a {@link MillisToHourTransform}
- * for {@link Duration} values.
  * 
  * 
  * @author Stephen Booth
- * @See {@link MillisToHourTransform}
- *
+ * @see HourTransform
  */
-public class HourTransform implements NumberTransform{
+public class MillisToHourTransform implements NumberTransform{
 	private String default_value="0:00:00";
 	
 	
-	public HourTransform(){
+	public MillisToHourTransform(){
 	}
 	
 	public String getDefaultValue() {
@@ -43,40 +37,12 @@ public class HourTransform implements NumberTransform{
 	public void setDefaultValue(String default_value) {
 		this.default_value = default_value;
 	}
-	/** Convert a {@link Number} of seconds into a HH:MM:SS string
-	 * 
-	 * @param d
-	 * @return
-	 */
-	 public static String toHrsMinSec(Number d) {
-         String result = "";
-         long value;
-         if( d instanceof Duration) {
-        	 value=((Duration)d).getSeconds();
-         }else {
-        	 value= d.longValue();
-         }
-         if( value < 0L){
-        	 result = result+"-";
-        	 value = -value;
-         }
-         long hours = (long) (value / 3600L);
-         result = result + hours + ":";
-         long minutes = (long) (value % 3600L) / 60L;
-         if (minutes < 10L)
-             result = result + "0";
-         result = result + minutes + ":";
-         long seconds = (long) (value % (60L));
-         if (seconds < 10L)
-             result = result + "0";
-         result = result + seconds;
-         return result;
-     }
+	
 	  public Object convert(Object old) {
           if (old != null) {
 
               if (old instanceof Number) {
-                  return toHrsMinSec((Number) old);
+                  return HourTransform.toHrsMinSec(((Number) old).longValue()/1000L);
               } else {
                   return old;
               }
