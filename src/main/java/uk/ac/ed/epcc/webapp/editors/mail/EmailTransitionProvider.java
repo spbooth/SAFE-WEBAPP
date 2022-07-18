543add0e5d9a7eb08f9798214bd40337287909f1
Stephen Booth <s.booth@epcc.ed.ac.uk>
2022-07-18 16:17:12 +0100

    fix size warning

--- src/main/java/uk/ac/ed/epcc/webapp/editors/mail/EmailTransitionProvider.java
+++ src/main/java/uk/ac/ed/epcc/webapp/editors/mail/EmailTransitionProvider.java
@@ -484,15 +484,7 @@ public class EmailTransitionProvider implements ViewPathTransitionProvider<EditA
 	@Override
 	public <X extends ContentBuilder> X getSummaryContent(AppContext c, X cb,
 			MailTarget target) {
-		if( target != null ) {
-			long max_email = c.getLongParameter("email.max_length", -1);
-			if( max_email > 0L ) {
-				long len = target.getMessageLength();
-				if( len > max_email) {
-					cb.getSpan("warn").clean("Message is too long and may not send").appendParent();
-				}
-			}
-		}
+		
 		return cb;
 	}
 	
@@ -602,7 +594,15 @@ public class EmailTransitionProvider implements ViewPathTransitionProvider<EditA
 		try {
 			MessageHandler handler = target.getHandler();
 			if( handler instanceof MessageComposer){
-				
+				if( target != null ) {
+					long max_email = c.getLongParameter("email.max_length", -1);
+					if( max_email > 0L ) {
+						long len = target.getMessageLength();
+						if( len > max_email) {
+							cb.getHeading(2).getSpan("warn").clean("Message is too long and may not send").appendParent().appendParent();
+						}
+					}
+				}
 				if( target.hashMatches()){
 					MessageComposerFormat mcf = new MessageComposerFormat(getContext(), (MessageComposer) handler, new TransitionLinker(getContext(), target));
 					mcf.getContent(cb);
