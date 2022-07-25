aab9b9f3c2998b97b7f1f923423496a066984df8
Stephen Booth <s.booth@epcc.ed.ac.uk>
2022-07-25 16:49:20 +0100

    order null check

--- src/main/java/uk/ac/ed/epcc/webapp/servlet/ClientLogServlet.java
+++ src/main/java/uk/ac/ed/epcc/webapp/servlet/ClientLogServlet.java
@@ -37,11 +37,12 @@ public class ClientLogServlet extends WebappServlet {
 				return;
 			}
 			String input = ss.getTextParameter(ServletService.DEFAULT_PAYLOAD_PARAM,true);
-			if( input.length() > conn.getIntegerParameter("client_log.max_message", 4096)) {
-				ss.sendError(HttpServletResponse.SC_BAD_REQUEST, "Message too long");
-				return;
-			}
+			
 			if( input != null ) {
+				if( input.length() > conn.getIntegerParameter("client_log.max_message", 4096)) {
+					ss.sendError(HttpServletResponse.SC_BAD_REQUEST, "Message too long");
+					return;
+				}
 				getLogger(conn).warn(input);
 			}
 		}
