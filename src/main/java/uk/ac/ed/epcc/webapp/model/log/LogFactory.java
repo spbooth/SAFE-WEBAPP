29592cf32a12a97bfa1432e9137c6e327196cedc
Stephen Booth <s.booth@epcc.ed.ac.uk>
2024-04-08 20:11:48 +0100

    remove support for factory less items

--- src/main/java/uk/ac/ed/epcc/webapp/model/log/LogFactory.java
+++ src/main/java/uk/ac/ed/epcc/webapp/model/log/LogFactory.java
@@ -486,10 +486,7 @@ public abstract class LogFactory<T extends LogFactory.Entry, O extends Indexed>
 		AppContext ctx = fac.getContext();
 		use_type=new ItemType<>(getStaticItemType());
 		if( DataObjectFactory.AUTO_CREATE_TABLES_FEATURE.isEnabled(ctx)){
-    		if( setContextWithMake(ctx, table,()->getDefaultTableSpecification(ctx,fac,uf,table))){
-    			// table created
-    			setupItems(ctx, table);
-    		}
+    		setContextWithMake(ctx, table,()->getDefaultTableSpecification(ctx,fac,uf,table));
     	}else{
     		setContext(ctx, table);
     	}
@@ -497,29 +494,7 @@ public abstract class LogFactory<T extends LogFactory.Entry, O extends Indexed>
 		user_factory = uf;
 		use_type.lock();
 	}
-	/** create LogItem tables that don't have factories.
-	 * 
-	 * @param c
-	 * @param table
-	 */
-	protected void setupItems(AppContext c,String table){
-		Map<String,TableSpecification> prereq=getPrereqTables();
 	
-		DataBaseHandlerService serv = c.getService(DataBaseHandlerService.class);
-		if( serv != null ){
-			for(String tab : prereq.keySet()){
-				if( ! serv.tableExists(tab)){
-					try {
-						serv.createTable(tab, prereq.get(tab));
-					} catch (DataFault e) {
-						getLogger().error("Error making prereq table",e);
-					}
-				}
-			}
-		}
-		
-		
-	}
 	/* (non-Javadoc)
 	 * @see uk.ac.ed.epcc.webapp.model.data.DataObjectFactory#observeComposite(uk.ac.ed.epcc.webapp.model.data.Composite)
 	 */
@@ -531,9 +506,7 @@ public abstract class LogFactory<T extends LogFactory.Entry, O extends Indexed>
 		}
 		
 	}
-	protected Map<String,TableSpecification> getPrereqTables(){
-		return new HashMap<>();
-	}
+	
 	protected TableSpecification getDefaultTableSpecification(AppContext c,LogOwner<O> fac,AppUserFactory<?>uf,String homeTable){
 		TableSpecification spec = new TableSpecification("LogID");
 		spec.setCurrentTag("LogFactory");
